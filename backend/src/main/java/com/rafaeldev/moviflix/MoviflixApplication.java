c3c91d81cd31027c87232525ef59553a30eb3eca
rafaelc <rafaelcouto@hotmail.com>
2020-12-21 00:22:32 -0300

    backend project created

---
+++ backend/src/main/java/com/rafaeldev/moviflix/MoviflixApplication.java
@@ -0,0 +1,13 @@
+package com.rafaeldev.moviflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviflixApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rafaeldev/moviflix/MoviflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldev.moviflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
